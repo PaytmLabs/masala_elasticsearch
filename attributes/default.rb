a12cfb28e770d44567fef57ff8f0f6493eb215f6
Joe Hohertz <jhohertz@gmail.com>
2017-11-28 13:43:59 -0500

    treat min master nodes as integer vs. string

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,6 +9,6 @@ default['masala_elasticsearch']['config']['node.attr.rack_id'] = 'default_rack'
 default['masala_elasticsearch']['config']['node.attr.zone_id'] = 'default_zone'
 default['masala_elasticsearch']['config']['path.data'] = '/elasticsearch/data'
 default['masala_elasticsearch']['config']['path.logs'] = '/elasticsearch/logs'
-default['masala_elasticsearch']['config']['discovery.zen.minimum_master_nodes'] = '2'
+default['masala_elasticsearch']['config']['discovery.zen.minimum_master_nodes'] = 2
 
 default['sysctl']['params']['vm']['max_map_count'] = "262144"
